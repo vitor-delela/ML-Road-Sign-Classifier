402856cda4f537377c6668e92f291357abf94700
vitor-delela <64380868+vitor-delela@users.noreply.github.com>
2023-12-01 17:38:13 -0300

    Update ModelPredict.py

--- ModelPredict.py
+++ ModelPredict.py
@@ -6,9 +6,8 @@ import tkinter as tk
 from tkinter import filedialog
 from PIL import Image, ImageTk
 
-model = load_model('./models/german_model_10epochs_rmsprop_comDropout_semDataAug.h5')
-# Carregar um arquivo CSV que mapeia índices de classe para nomes de classe
-class_mapping = pd.read_csv('/Users/vitordelela/Documents/PUCRS/7 SEM/AM/T2/German_Dataset/signname.csv') 
+model = load_model('./models/model_10epochs_rmsprop_dropout.h5')
+class_mapping = pd.read_csv('./German_Dataset/signname.csv') 
 
 def load_image():
     file_path = filedialog.askopenfilename(filetypes=[("Images", ["*.jpg", "*.png", "*.jpeg"])])
@@ -24,58 +23,48 @@ def load_image():
 
         phrase_label.config(text="Carregando...")
 
-        # image_path = '/Users/vitordelela/Downloads/ice.jpg'
         try:
             img = Image.open(file_path)
         except Exception as e:
-            print(f"Erro ao abrir a imagem: {e}")
+            print(f"Error: {e}")
 
-        # Verificar o modo da imagem (deve ser 'RGB')
         if img.mode != 'RGB':
             img = img.convert('RGB')
 
-        # Remover o canal alfa se estiver presente
         img = img.convert('RGB')
 
-        # Redimensionar a imagem para o formato do seu modelo (32, 32, 3)
+        # Resize image to (32, 32, 3)
         img = img.resize((32, 32))
         img_array = np.array(img)
-        img_array = img_array.astype('float32') / 255.0 # Normalizar os valores para o intervalo [0, 1]
+        img_array = img_array.astype('float32') / 255.0
         img_array = np.expand_dims(img_array, axis=0)
 
         prediction = model.predict(img_array)
 
-        top_indices = np.argsort(prediction[0])[-3:][::-1] # Obter os índices das três maiores probabilidades
-        top_classes = class_mapping.loc[top_indices, 'SignName'].values # Mapear os índices para os nomes das classes usando o arquivo CSV
+        top_indices = np.argsort(prediction[0])[-3:][::-1] # Getting top 3 prob
+        top_classes = class_mapping.loc[top_indices, 'SignName'].values
 
         # Obter as três maiores probabilidades
         top_probabilities = prediction[0][top_indices]
         predicted_class_index = np.argmax(prediction)
 
-        retorno = ''
-        # Imprimir os índices e probabilidades
+        _return = ''
         for i, (class_name, probability) in enumerate(zip(top_classes, top_probabilities), 1):
-            retorno += f'Top {i}: {class_name} - probabilidade {probability:.2%}\n'
-            print(f'Top {i}: Classe {class_name} - probabilidade {probability:.2%}')
+            _return += f'Top {i}: {class_name} - Probability {probability:.2%}\n'
+            print(f'Top {i}: Class {class_name} - Probability {probability:.2%}')
 
         phrase_label.config(text=retorno)
 
-# Cria a janela principal
-root = tk.Tk()
-root.title("Classificador")
 
-# Cria uma label para exibir a imagem
+root = tk.Tk()
+root.title("Classifier")
 image_label = tk.Label(root)
 image_label.pack(padx=10, pady=10)
-
 phrase_label = tk.Label(root, text="-", font=("Helvetica", 14))
 phrase_label.pack(pady=10)
-
-# Botão para selecionar uma imagem
-load_button = tk.Button(root, text="Carregar Imagem", command=load_image)
+load_button = tk.Button(root, text="Load Image", command=load_image)
 load_button.pack(pady=10)
 
-# Inicia o loop principal da interface gráfica
 root.mainloop()
 
 
